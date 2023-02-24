cfe076c276a79cd2c9bbc52191e528e32b9b2701
Grigoryashka <vdjwhwjbw@gmail.com>
2023-02-24 15:53:02 +0500

    someshit

--- classwork/Program.cs
+++ classwork/Program.cs
@@ -28,7 +28,7 @@ PrintMatrix(matrix);*/
 
 
 // TASK 48
-void InputMatrix(int[,] matrix)
+/*void InputMatrix(int[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
@@ -63,5 +63,61 @@ int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
 int[,] matrix = new int[size[0], size[1]];
 InputMatrix(matrix);
 PrintMatrix(matrix);
-Console.Write($"Сумма диагонали: {SummaryOfDiagonal(matrix)}");
+Console.Write($"Сумма диагонали: {SummaryOfDiagonal(matrix)}");*/
 
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(1, 21); // [1, 20]
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++){
+            if (matrix[i, j] != 0) Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+/*void ReplaceFirstLastRow(int[,] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        int temp = matrix[0, j];
+        matrix[0, j] = matrix[matrix.GetLength(0) - 1, j];
+        matrix[matrix.GetLength(0) - 1, j] = temp;
+    }
+}*/
+void ReplaceFirstLastRow(int[,] matrix)
+{
+    int[] uniq = new int [matrix.GetLength(0) * matrix.GetLength(1)];
+    //int min = matrix[0, 0];
+    //int imin = 0, jmin = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            int? g = Array.Find(uniq, x => x == matrix[i, j]);
+            //Console.Write($"{g}");
+            if (g == 0) {uniq.Append(matrix[i, j]);}
+        }
+    }
+    foreach (int i in uniq) Console.Write($"{i} ");
+}
+Console.Clear();
+Console.Write("Введите размер массива: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+Console.WriteLine("Начальный массив:");
+InputMatrix(matrix);
+PrintMatrix(matrix);
+ReplaceFirstLastRow(matrix);
+Console.WriteLine("Конечный массив: ");
+PrintMatrix(matrix);
